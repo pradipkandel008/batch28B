6b485b8b899f7ae31d82e0fd6ec04b28ebd7e439
pradipkandel008 <pradeepkandel008@gmail.com>
2021-06-28 13:39:13 +0545

    javascript

---
+++ javascript/externaljs.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  console.log("Button clicked!");
+  window.alert("Button clicked");
+}
+
+// document.write("This is text from external javascript");
+
+document.getElementById("para1").innerHTML = "Paragraph content.";
